696157460f271e2134d5fb522566baa2157d1ba8
Naim Hasan <naimislam24ff@gmail.com>
2026-09-27 00:23:36 +0600

    update file

--- src/app/lib/cards.ts
+++ src/app/lib/cards.ts
@@ -1,5 +1,6 @@
 export const getAllCard = async () => {
-  const red = await fetch("http://localhost:4000/card", {
+  // const red = await fetch("http://localhost:4000/card", {
+  const red = await fetch("https://api.api-store.workers.dev/api/fitlog", {
     // next: { revalidate: 10 },
   });
   const data = await red.json();
